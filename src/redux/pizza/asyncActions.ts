235b34be3aa4d137f8671d53ff79ef422b966dfd
Shevchenko Roman <shevchromaha@gmail.com>
2025-12-10 17:16:07 +0200

    reactshop

---
+++ src/redux/pizza/asyncActions.ts
@@ -0,0 +1,31 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+import { Pizza, SearchPizzaParams } from "./types";
+import pickBy from "lodash/pickBy";
+import identity from "lodash/identity";
+
+export const fetchPizzas = createAsyncThunk<Pizza[], SearchPizzaParams>(
+  "pizza/fetchPizzasStatus",
+  async (params) => {
+    const { sortBy, order, category, search, currentPage } = params;
+
+    const { data } = await axios.get<Pizza[]>(
+      `https://6790b6a5af8442fd73775b02.mockapi.io/pizzas`,
+      {
+        params: pickBy(
+          {
+            page: currentPage,
+            limit: 4,
+            category,
+            sortBy,
+            order,
+            search,
+          },
+          identity
+        ),
+      }
+    );
+
+    return data;
+  }
+);
