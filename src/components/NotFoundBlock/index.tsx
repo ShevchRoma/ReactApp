4a4115218942c29ce020d659ceb2a59b1487d5a8
Shevchenko Roman <shevchromaha@gmail.com>
2025-12-11 09:48:01 +0200

    refactoring

--- src/components/NotFoundBlock/index.tsx
+++ src/components/NotFoundBlock/index.tsx
@@ -1,16 +1,16 @@
-import React from 'react';
+import React from "react";
 
-import styles from './NotFoundBlock.module.scss';
+import "./NotFoundBlock.module.scss";
 
 export const NotFoundBlock: React.FC = () => {
   return (
-    <div className={styles.root}>
+    <div className="not-found">
       <h1>
         <span>😕</span>
         <br />
         Ничего не найдено
       </h1>
-      <p className={styles.description}>
+      <p className="not-found__description">
         К сожалени данная страница отсутствует в нашем интернет-магазине
       </p>
     </div>
